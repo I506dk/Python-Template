ec07683bdd16b19fa7a4baf202a02a47d9d921a7
I506dk <33561466+I506dk@users.noreply.github.com>
2023-06-14 13:57:53 -0500

    Update example_script.py

--- example_script.py
+++ example_script.py
@@ -13,21 +13,42 @@ def parse():
     )
     
     # Add argument 1
-    parser.add_argument("-a1", "--argument_1", dest="argument_1", action="store",
-        type=str, required=False, help="The first argument of the script.")
+    parser.add_argument(
+        "-a1",
+        "--argument_1",
+        dest="argument_1",
+        action="store",
+        type=str,
+        required=False,
+        help="The first argument of the script."
+    )
     
     # Add argument 2
-    parser.add_argument("-a2", "--argument_2", dest="argument_2", action="store",
-        type=str, required=False, help="The second argument of the script.")
+    parser.add_argument(
+        "-a2",
+        "--argument_2",
+        dest="argument_2",
+        action="store",
+        type=str,
+        required=False,
+        help="The second argument of the script."
+    )
    
     # Add argument 3
-    parser.add_argument("-a3", "--argument_3", dest="argument_3", action="store",
-        type=str, required=False, help="The third argument of the script.")
+    parser.add_argument(
+        "-a3",
+        "--argument_3",
+        dest="argument_3",
+        action="store",
+        type=str,
+        required=False,
+        help="The third argument of the script."
+    )
 
     return parser.parse_args()
 
 # Beginning of main
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Get command line arguments
     args = parse()
 
@@ -40,11 +61,17 @@ if __name__ == '__main__':
     for key in argument_keys:
         if getattr(args, key) is None:
             if key == "argument_1":
-                args.argument_1 = input("Please enter the first argument for the script: ")
+                args.argument_1 = input(
+                    "Please enter the first argument for the script: "
+                )
             if key == "argument_2":
-                args.argument_2 = input("Please enter the second argument for the script: ")
+                args.argument_2 = input(
+                    "Please enter the second argument for the script: "
+                )
             if key == "argument_3":
-                args.argument_3 = input("Please enter the third argument for the script: ")
+                args.argument_3 = input(
+                    "Please enter the third argument for the script: "
+                )
                 
     # Print out the passed arguments
     print(f"Here is argument 1: {args.argument_1}")
